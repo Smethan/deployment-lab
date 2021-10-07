631aa003e72257f51cc98567e158486823c61659
Smethan <ethanhargus@gmail.com>
2021-10-07 14:17:34 -0500

    layed out html

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.get('/', (req,res) => {
     res.sendFile(new URL('../index.html', import.meta.url).pathname)
 })
 
+app.get('/css', (req,res) => {
+    res.sendFile(new URL('../index.css', import.meta.url).pathname)
+})
+
 const port = process.env.PORT || 4040;
 
 app.listen(port, () => {

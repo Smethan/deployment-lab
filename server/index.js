f48baebe80a68e8776e10135a547b254f3216072
Smethan <ethanhargus@gmail.com>
2021-10-07 13:48:49 -0500

    fixed dirname error

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import path from 'path'
+import {path, dirname} from 'path'
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 

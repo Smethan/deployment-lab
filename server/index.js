8bc4fb1bcb1af670a489f6c10b5a47fb4b3df13b
Smethan <ethanhargus@gmail.com>
2021-10-07 14:43:19 -0500

    I hope this works

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import path from 'path'
-import { URL } from 'url';
+import { URL, fileURLToPath, pathToFileURL } from 'url';
 
 const __dirname = new URL('.', import.meta.url).pathname
+const __filename = fileURLToPath(import.meta.url);
 
 const app = express();
 
@@ -10,9 +11,7 @@ app.get('/', (req,res) => {
     res.sendFile(new URL('../index.html', import.meta.url).pathname)
 })
 
-app.get('/css', (req,res) => {
-    res.sendFile(new URL('../index.css', import.meta.url).pathname)
-})
+app.use('/css', express.static(path.join(__dirname, '../index.css')))
 
 const port = process.env.PORT || 4040;
 
